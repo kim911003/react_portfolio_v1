e288f96bacf712a8db19ca90f9fae4fc9b1466ad
kim911003 <joon1077@naver.com>
2021-09-02 09:34:44 +0900

    Updates

---
+++ precache-manifest.a2670b3d87107d85a2a9c5987be0c7cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ebac49391aa5ed4e4ac",
+    "url": "/react_portfolio_v1/static/css/main.4b096992.chunk.css"
+  },
+  {
+    "revision": "9ebac49391aa5ed4e4ac",
+    "url": "/react_portfolio_v1/static/js/main.9ebac493.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react_portfolio_v1/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "d9be8839afd377a24aaf",
+    "url": "/react_portfolio_v1/static/js/runtime~main.d9be8839.js"
+  },
+  {
+    "revision": "f59233e6bbe2ef0f4389f1fd40082cae",
+    "url": "/react_portfolio_v1/index.html"
+  }
+];
